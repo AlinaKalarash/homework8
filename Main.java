5c8621f58cd115401ac1a4c90e833b430eed7527
AlinaKalarash <140280974+AlinaKalarash@users.noreply.github.com>
2023-07-24 04:32:25 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -7,9 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter name of shape: ");
-        Shape shape = new Shape();
-        String s = scanner.nextLine();
-        shape.printInfo(s);
+        Shape.printInfo(scanner.nextLine());
         
         scanner.close();
     }
